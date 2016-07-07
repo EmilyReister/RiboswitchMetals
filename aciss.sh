3e1b0d758663f882cab818fa98d249a9cb402dd5
EmilyReister <ereister@uoregon.edu>
2016-07-07 13:05:18 -0700

    Update aciss.sh

--- aciss.sh
+++ aciss.sh
@@ -4,10 +4,8 @@ module load blast
 module load python
 module load clustal-omega/1.2.1
 
+#Change this directory to lead to the location of YOUR run.sh script
 DIR='/home7/ereister/Bioinformatics_Spring2016/scripts'
 
-#cd $DIR
-#./run.sh
-
 cd $DIR
-./cmsc.sh
+./run.sh
